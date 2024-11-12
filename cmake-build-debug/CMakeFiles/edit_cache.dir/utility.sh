0fb776ed2c4ee13687a8056390e5eca1500a6998
Yujinia <76672931+yujinia95@users.noreply.github.com>
2024-11-12 01:06:20 -0800

    Delete cmake-build-debug directory

--- cmake-build-debug/CMakeFiles/edit_cache.dir/utility.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-cd /cygdrive/c/Users/te020/Desktop/CLion_C/Lab6/cmake-build-debug
-/cygdrive/c/Users/te020/AppData/Local/JetBrains/CLion2024.2/cygwin_cmake/bin/cmake.exe -E echo No\ interactive\ CMake\ dialog\ available.
